3db475a3008dcd46f44111c930b5ceb32172b5c6
balister <balister@eccf9777-9b30-0410-a39f-6f59b0ce54fe>
2009-04-23 21:07:03 +0000

    Pins are configured. SPI test code.
    git-svn-id: https://svn.geekisp.com/opensdr@301 eccf9777-9b30-0410-a39f-6f59b0ce54fe

--- beagle-sdr/Beat-army/top_level.v
+++ beagle-sdr/Beat-army/top_level.v
@@ -8,20 +8,20 @@ input pin4,
 input pin5,
 input pin6,
 input pin7,
-input pin8, // MCSPI3_CS0
+input pin8,
 input pin9,
-input pin10, // MCSPI3_CS1
-input pin11,
+input pin10,
+input pin11, // MCSPI3_CS0
 input pin12,
-input pin13,
-input pin14, // MCSPI3_SOMI
+input pin13, // MCSPI3_CS1
+input pin14,
 input pin15,
-input pin16, // MCSPI3_SIMO
-input pin17,
-input pin18, // MCSPI3_CLK
-input pin19,
+input pin16,
+output pin17, // MCSPI3_SOMI
+input pin18,
+input pin19, // MCSPI3_SIMO
 input pin20,
-input pin21,
+input pin21, // MCSPI3_CLK
 input pin22,
 input pin23,
 input pin24,
@@ -43,9 +43,30 @@ inout wire [15:0] io_tx_a,
 inout wire [15:0] io_rx_a
 );
 
-assign io_rx_a[0] = pin8;
-assign io_rx_a[1] = pin10;
-assign io_rx_a[2] = pin16;
-assign io_rx_a[3] = pin18;
+assign io_rx_a[0] = pin3;
+assign io_rx_a[1] = pin4;
+assign io_rx_a[2] = pin23;
+assign io_rx_a[3] = pin24;
+assign io_rx_a[4] = pin14;
+assign io_rx_a[5] = pin15;
+assign io_rx_a[6] = pin16;
+assign io_rx_a[7] = pin17;
+assign io_rx_a[8] = pin18;
+assign io_rx_a[9] = pin19;
+assign io_rx_a[10] = pin20;
+assign io_rx_a[11] = pin21;
+assign io_rx_a[12] = pin22;
+assign io_rx_a[13] = pin23;
+assign io_rx_a[14] = pin24;
+
+assign led[0] = 1;
+
+reg rx_data;
+always @(negedge pin21)
+begin
+    rx_data = ! rx_data;
+end
+
+assign pin17 = rx_data;
 
 endmodule
