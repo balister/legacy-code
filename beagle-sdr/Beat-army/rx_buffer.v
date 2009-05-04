4d5c3ed39dd99a7be2b11d6d14e3234169592355
balister <balister@eccf9777-9b30-0410-a39f-6f59b0ce54fe>
2009-05-04 23:50:44 +0000

    Work in progress
    git-svn-id: https://svn.geekisp.com/opensdr@303 eccf9777-9b30-0410-a39f-6f59b0ce54fe

--- beagle-sdr/Beat-army/rx_buffer.v
+++ beagle-sdr/Beat-army/rx_buffer.v
@@ -2,8 +2,8 @@
 
 module rx_buffer (
 	input spi_clk,
-	output spi_simo,
-	input spi_somi,
+	output spi_output,
+	input spi_input,
 	input spi_cs0,
 	input spi_cs1,
 
@@ -19,7 +19,7 @@ module rx_buffer (
 	input wire [15:0] ch_2,
 	input wire [15:0] ch_3,
 
-	input rxclk,
+	input rx_clk,
 	input rxstrobe,
 	input clear_status,
 
@@ -27,8 +27,86 @@ module rx_buffer (
 	input [31:0] serial_data,
 	input serial_strobe,
 
-	output [31:0] debugbus
+	output [31:0] debug_bus
 );
 
+wire [31:0] fifo_data;
+wire [31:0] spi_data;
+reg read_fifo;
+reg write_fifo;
+wire [11:0] rd_fifo_level;
+wire [11:0] wr_fifo_level;
+wire wr_fifo_full;
+
+// Instantiate FIFO
+
+rx_fifo	rx_fifo_inst (
+	.data (fifo_data),
+	.rdclk (spi_clk),
+	.rdreq (!read_fifo),
+	.wrclk (rx_clk),
+	.wrreq (fill_fifo),
+	.q (spi_data),
+	.rdempty ( ),
+	.rdfull ( ),
+	.rdusedw (rd_fifo_level ),
+	.wrempty (),
+	.wrfull (wr_fifo_full),
+	.wrusedw (wr_fifo_level)
+	);
+
+
+// Read from FIFO and feed to SPI
+
+reg [31:0] q_to_spi;
+reg [5:0] bit_cnt;
+
+assign spi_output = q_to_spi[0];
+
+always @(negedge spi_clk)
+begin
+	if (bit_cnt == 6'd31) begin
+		bit_cnt <= 6'b0;
+		q_to_spi <= spi_data;
+	end else begin
+		bit_cnt <= bit_cnt + 6'b1; 
+		q_to_spi <= {1'b0, q_to_spi[31:1]};
+	end
+end
+
+always @(posedge spi_clk)
+begin
+	if (bit_cnt == 6'd31)
+		read_fifo <= 1'b1;
+	else
+		read_fifo <= 1'b0;
+end
+
+assign debug_bus[0] = spi_clk;
+assign debug_bus[1] = spi_output;
+assign debug_bus[7:2] = bit_cnt;
+assign debug_bus[8] = read_fifo;
+
+// Write data into FIFO
+
+reg fill_fifo;
+reg [31:0]rx_data;
+
+always @(negedge rx_clk)
+begin
+	rx_data <= 32'hDEADBEEF;
+
+	if (wr_fifo_full)
+		fill_fifo <= 0;
+	else if (wr_fifo_level < 1024)
+		fill_fifo <= 1;
+	else
+		fill_fifo <= fill_fifo;
+
+end
+
+assign debug_bus[9] = fill_fifo;
+assign debug_bus[10] = wr_fifo_full;
+assign debug_bus[11] = rx_clk;
 
 endmodule
--- beagle-sdr/Beat-army/top_level.v
+++ beagle-sdr/Beat-army/top_level.v
@@ -44,59 +44,42 @@ inout wire [15:0] io_rx_a
 );
 
 wire host_spi_clk;
-wire host_spi_simo;
-wire host_spi_somi;
+wire host_spi_input;
+wire host_spi_output;
 wire host_spi_cs0;
 wire host_spi_cs1;
 
 assign host_spi_clk = pin21;
-assign host_spi_simo = pin19;
-assign host_spi_somi = pin17;
+assign host_spi_input = pin19;
+assign host_spi_output = pin17;
 assign host_spi_cs0 = pin11;
 assign host_spi_cs1 = pin13;
 
+wire [15:0]rx_debug_bus;
 
+wire clk64;
 
-assign io_rx_a[0] = pin3;
-assign io_rx_a[1] = pin4;
-assign io_rx_a[2] = pin23;
-assign io_rx_a[3] = pin24;
-assign io_rx_a[4] = pin14;
-assign io_rx_a[5] = pin15;
-assign io_rx_a[6] = pin16;
-assign io_rx_a[7] = pin17;
-assign io_rx_a[8] = pin18;
-assign io_rx_a[9] = pin19;
-assign io_rx_a[10] = pin20;
-assign io_rx_a[11] = pin21;
-assign io_rx_a[12] = pin22;
-assign io_rx_a[13] = pin23;
-assign io_rx_a[14] = pin24;
+assign io_rx_a = rx_debug_bus;
+
+assign clk64 = master_clk;
 
 assign led[0] = 1;
 
-reg rx_data;
-always @(negedge pin21)
-begin
-    rx_data = ! rx_data;
-end
 
-assign pin17 = rx_data;
-
-//`define FOO
+`define FOO
 `ifdef FOO
 rx_buffer rx_buffer
-	  ( .spi_clk(host_spi_clk), .spi_simo(host_spi_clk), .spi_somi(host_spi_somi),
+	  ( .spi_clk(host_spi_clk), .spi_input(host_spi_input), .spi_output(host_spi_output),
 		.spi_cs0(host_spi_cs0), .spi_cs1(host_spi_cs1),
-		.bus_reset(rx_bus_reset),.reset(rx_dsp_reset), .reset_regs(rx_dsp_reset),
-		.RD(RD),.have_pkt_rdy(have_pkt_rdy),.rx_overrun(rx_overrun),
-		.channels(rx_numchan),
-		.ch_0(ch0rx),.ch_1(ch1rx),
-		.ch_2(ch2rx),.ch_3(ch3rx),
-		.rxclk(clk64),.rxstrobe(hb_strobe),
-		.clear_status(clear_status),
-		.serial_addr(serial_addr),.serial_data(serial_data),.serial_strobe(serial_strobe),
-		.debugbus(rx_debugbus) );
+		.reset(), .reset_regs(),
+		.have_pkt_rdy(),.rx_overrun(),
+		.channels(),
+		.ch_0(),.ch_1(),
+		.ch_2(),.ch_3(),
+		.rx_clk(clk64),.rxstrobe(),
+		.clear_status(),
+		.serial_addr(),.serial_data(),.serial_strobe(),
+		.debug_bus(rx_debug_bus) );
 `endif
 
 endmodule
